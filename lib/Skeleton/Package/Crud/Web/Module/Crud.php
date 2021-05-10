54b99fdfde5c1cd32a921f9f03e8bfff4070a1e5
Christophe Gosiau <christophe@tigron.be>
2021-05-10 11:05:51 +0200

    prepare for new skeleton-core

--- lib/Skeleton/Package/Crud/Web/Module/Crud.php
+++ lib/Skeleton/Package/Crud/Web/Module/Crud.php
@@ -10,7 +10,7 @@
 namespace Skeleton\Package\Crud\Web\Module;
 
 use \Skeleton\Core\Web\Template;
-use \Skeleton\Core\Web\Module;
+use \Skeleton\Core\Application\Web\Module;
 use \Skeleton\Core\Web\Session;
 use \Skeleton\Pager\Web\Pager;
 
