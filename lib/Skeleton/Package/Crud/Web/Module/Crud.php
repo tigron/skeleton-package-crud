8efb171d7d22da2159146d6fd474aeef6db6025b
Christophe Gosiau <christophe@tigron.be>
2022-11-30 09:52:59 +0100

    compatibility with skeleton-core 4

--- lib/Skeleton/Package/Crud/Web/Module/Crud.php
+++ lib/Skeleton/Package/Crud/Web/Module/Crud.php
@@ -10,7 +10,7 @@
 namespace Skeleton\Package\Crud\Web\Module;
 
 use \Skeleton\Core\Web\Template;
-use \Skeleton\Core\Application\Web\Module;
+use \Skeleton\Application\Web\Module;
 use \Skeleton\Core\Web\Session;
 use \Skeleton\Pager\Web\Pager;
 
